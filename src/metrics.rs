6aca782011a14665379455828c93aca01842acd9
youthon.yang <youthon.yang@webeye.cn>
2024-11-21 15:36:04 +0800

    move get_setting to pub

--- src/metrics.rs
+++ src/metrics.rs
@@ -147,7 +147,7 @@ const DEFAULT_HISTOGRAM_BUCKETS: [f64; 14] = [
     0.005, 0.01, 0.025, 0.05, 0.075, 0.1, 0.25, 0.5, 0.75, 1.0, 2.5, 5.0, 7.5, 10.0,
 ];
 
-pub(crate) fn get_settings() -> &'static GlobalSettings {
+pub fn get_settings() -> &'static GlobalSettings {
     GLOBAL_SETTINGS.get_or_init(Default::default)
 }
 
